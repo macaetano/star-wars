ba6acf1a3016dd147319c4e2c41a44cc9dc357a5
Matheus De Assis Caetano <matheusdeassiscaetano@gmail.com>
2021-02-17 09:06:47 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>How many stops would you do?</h1>
+
+      <div>
+        <h2>Label</h2>
+        <div>
+          <input type="number" placeholder="Placeholder" />
+          <button>Search</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
